fa0a5b7647d0349846832cb639a8689397eff22b
Jing Li <jing_li1998@hotmail.com>
2019-11-06 16:08:41 +1100

    Add comment and user models

--- models/dog.js
+++ models/dog.js
@@ -8,7 +8,20 @@ var dogSchema = new mongoose.Schema({
   age: String,
   amount: String,
   description: String,
-  created: {type: Date, default: Date.now}
+  created: {type: Date, default: Date.now},
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    }
+  ],
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  }
 });
 
 module.exports = mongoose.model("Dog", dogSchema);
