20ff1b47c686800095f0ebc02fe5ed3266a42995
Jing Li <jing_li1998@hotmail.com>
2019-11-12 08:35:54 +1100

    add signup to databse functionality

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const express = require('express'),
       expressSanitizer = require("express-sanitizer"),
       passport = require("passport"),
       LocalStrategy = require("passport-local"),
+      User = require('./models/user'),
       flash = require("connect-flash"),
-      seedDB = require("./seed");
+      seedDB = require("./seed"),
+      session = require("express-session"),
+      MemoryStore = require("session-memory-store")(session);
 
 // ROUTES
 const commentRoutes = require("./routes/comments"),
@@ -34,6 +37,25 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  name: "JSESSION",
+  secret: "Luna Gao",
+  store: new MemoryStore(),
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
 
 // SEED DB IF REQUIRED
 // seedDB();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express"),
-      router = express.Router();
-      // passport = require("passport"),
-      // User = require("../models/user");
+      router = express.Router(),
+      passport = require("passport"),
+      User = require("../models/user");
 
 // INDEX ROUTE
 router.get('/', (req, res) => {
@@ -13,14 +13,34 @@ router.get('/about', (req, res) => {
   res.send("You have reached the about route");
 });
 
+// SIGNUP ROUTE
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+// SIGNUP LOGIC
+router.post('/signup', (req, res) => {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      req.flash("error", err.message);
+      return res.render('signup');
+    }
+    passport.authenticate('local')(req, res, function() {
+      req.flash("success", "Welcome to Dbay " + user.username);
+      res.redirect('/dogs');
+    });
+  });
+});
+
 // LOGIN ROUTE
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// SIGNUP ROUTE
-router.get('/signup', (req, res) => {
-  res.render('signup');
+// LOGIN LOGIC
+router.post('/login', (req, res) => {
+  res.send('You have been logged in');
 });
 
 module.exports = router;
