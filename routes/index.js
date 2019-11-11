aef6648018f960c7e39da74e06ec583b287cdb2f
Jing Li <jing_li1998@hotmail.com>
2019-11-12 00:27:37 +1100

    Begin adding login and signup functionality

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,14 @@ router.get('/about', (req, res) => {
   res.send("You have reached the about route");
 });
 
+// LOGIN ROUTE
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// SIGNUP ROUTE
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
 module.exports = router;
